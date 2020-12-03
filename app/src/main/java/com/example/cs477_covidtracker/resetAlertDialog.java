531cf56623e5460e1c1eb3a0d219401d3e3433e8
Brandon Tran <btran20@masonlive.gmu.edu>
2020-12-03 00:51:58 -0500

    Added reset alert dialog and created buttons for settings

---
+++ app/src/main/java/com/example/cs477_covidtracker/resetAlertDialog.java
@@ -0,0 +1,44 @@
+package com.example.cs477_covidtracker;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.fragment.app.DialogFragment;
+
+//For unsaved changes in add/edit workouts.
+public class resetAlertDialog extends DialogFragment {
+    public static SharedPreferences sharedPref;
+    public static resetAlertDialog newInstance(String title){
+        DialogFragment dialog = new resetAlertDialog();
+        Bundle args = new Bundle();
+        args.putString("title", "Unsaved Changes");
+        dialog.setArguments(args);
+        return (resetAlertDialog) dialog;
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState){
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        sharedPref = this.getActivity().getSharedPreferences("pref", Context.MODE_PRIVATE);
+        builder.setMessage("You are resetting all settings to default and erasing all preferences. Do you want to continue?")
+                .setPositiveButton("Reset", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                         sharedPref.edit().clear().commit();
+                        Toast.makeText(getContext(), "Settings reset", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                    }
+                });
+        return builder.create();
+    }
+}
--- app/src/main/java/com/example/cs477_covidtracker/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/example/cs477_covidtracker/ui/notifications/NotificationsFragment.java
@@ -1,35 +1,83 @@
 package com.example.cs477_covidtracker.ui.notifications;
 
+import android.media.Image;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.cs477_covidtracker.R;
+import com.example.cs477_covidtracker.resetAlertDialog;
 
 public class NotificationsFragment extends Fragment {
 
     private NotificationsViewModel notificationsViewModel;
 
+
+    ImageButton darkMode, reset, changeLocation;
+    private boolean isdarkMode = false;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         notificationsViewModel =
                 ViewModelProviders.of(this).get(NotificationsViewModel.class);
         View root = inflater.inflate(R.layout.fragment_notifications, container, false);
-        final TextView textView = root.findViewById(R.id.text_notifications);
+        /*final TextView textView = root.findViewById(R.id.text_notifications);
         notificationsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
             }
+        });*/
+       darkMode = root.findViewById(R.id.darkmode);
+
+        darkMode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!isdarkMode) {
+                    isdarkMode = true;
+                    AppCompatDelegate
+                            .setDefaultNightMode(
+                                    AppCompatDelegate
+                                            .MODE_NIGHT_YES);
+                }
+                else{
+                    isdarkMode = false;
+                    AppCompatDelegate
+                            .setDefaultNightMode(
+                                    AppCompatDelegate
+                                            .MODE_NIGHT_NO);
+                }
+            }
+        });
+
+        reset = root.findViewById(R.id.reset);
+
+        reset.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                FragmentManager fm = getFragmentManager();
+                resetAlertDialog dialog = resetAlertDialog.newInstance("Reset");
+                dialog.show(fm, "Reset");
+            }
         });
         return root;
     }
+
+    public void resetAlert(View root){
+
+    }
+    //public void toggleDarkMode(View v){
+
+    //}
 }
